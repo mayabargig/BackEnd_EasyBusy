e30a1324b5e4c28b86598fbf5a35b995c730efba
mayabargig <mayabargig@gmail.com>
2024-05-23 01:23:53 +0300

    upload to heroku

--- app.js
+++ app.js
@@ -13,13 +13,21 @@ const cartRouter = require("./routes/cart.routes")
 const cors = require("cors");
 
 const app = express();
-app.use(cors(
+// app.use(cors(
 
-    {
-        origin: ['https://mayabargig.github.io', 'https://easy-busy-50e71066dde3.herokuapp.com'],
-        optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-    }
-));
+//     {
+//         origin: ['https://mayabargig.github.io', 'https://easy-busy-50e71066dde3.herokuapp.com'],
+//         optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+//     }
+// ));
+
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true, // Allow cookies to be sent
+    optionsSuccessStatus: 200 // For legacy browser support
+}));
 app.use(express.json());
 
 app.use("/api/v1/users", usersRouter);
