840c553e9ebc709d11aa9acede0ef12d5c464b9d
mayabargig <mayabargig@gmail.com>
2024-05-31 23:24:39 +0300

    upload to heroku

--- app.js
+++ app.js
@@ -14,24 +14,26 @@ const app = express();
 
 // Specify allowed origins
 const allowedOrigins = [
-  'https://mayabargig.github.io',
-  'https://easy-busy-50e71066dde3.herokuapp.com'
+    'https://mayabargig.github.io',
+    'https://easy-busy-50e71066dde3.herokuapp.com'
 ];
-
+const isProduction = process.env.NODE_ENV === 'production';
 app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-  credentials: true, // Allow cookies to be sent
-  optionsSuccessStatus: 200 // For legacy browser support
+    origin: isProduction ? (origin, callback) => {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    } : '*',
+    // origin: 'https://mayabargig.github.io',
+    // origin: 'https://easy-busy-50e71066dde3.herokuapp.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],    // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true, // Allow cookies to be sent
+    optionsSuccessStatus: 200 // For legacy browser support
 }));
 
 app.use(express.json());
