3757c1f7cc7df3a9791a0c0d649357b11541cc7b
mayabargig <mayabargig@gmail.com>
2024-05-31 22:29:50 +0300

    upload to heroku

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
-// const bodyParser = require('body-parser');
-// const nodemailer = require('nodemailer');
+const cors = require("cors");
 
 const usersRouter = require("./routes/users.routes");
 // const adminRouter = require("./routes/admin.routes");
@@ -9,25 +8,32 @@ const businessRouter = require("./routes/business.routes");
 const favoritesPRouter = require("./routes/favoriteP.routes");
 const paymentRouter = require("./routes/payments.routes");
 const appointmentRouter = require("./routes/appointments.routes");
-const cartRouter = require("./routes/cart.routes")
-const cors = require("cors");
+const cartRouter = require("./routes/cart.routes");
 
 const app = express();
-// app.use(cors(
 
-//     {
-//         origin: ['https://mayabargig.github.io', 'https://easy-busy-50e71066dde3.herokuapp.com'],
-//         optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-//     }
-// ));
+// Specify allowed origins
+const allowedOrigins = [
+  'https://mayabargig.github.io',
+  'https://easy-busy-50e71066dde3.herokuapp.com'
+];
 
 app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-    credentials: true, // Allow cookies to be sent
-    optionsSuccessStatus: 200 // For legacy browser support
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  credentials: true, // Allow cookies to be sent
+  optionsSuccessStatus: 200 // For legacy browser support
 }));
+
 app.use(express.json());
 
 app.use("/api/v1/users", usersRouter);
